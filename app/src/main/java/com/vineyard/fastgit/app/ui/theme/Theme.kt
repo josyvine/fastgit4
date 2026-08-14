72f1c3c568f07afa1369457fd612cc9ad570bac6
josyvine <64621103+josyvine@users.noreply.github.com>
2026-08-15 00:50:56 +0530

    Update Theme.kt

--- app/src/main/java/com/vineyard/fastgit/app/ui/theme/Theme.kt
+++ app/src/main/java/com/vineyard/fastgit/app/ui/theme/Theme.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = GhAccentBlue,
@@ -47,6 +48,7 @@ fun FastGitTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
